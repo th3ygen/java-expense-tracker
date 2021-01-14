b32552c18e47c566cb7bcdf340f427713aa5f755
th3ygen <th3ygen@gmail.com>
2021-01-14 19:40:02 +0800

    modulated interface

--- src/App.java
+++ src/App.java
@@ -1,5 +1,19 @@
+import ui.StartPage;
+
 public class App {
+    private static StartPage START_PAGE;
+
+    private static void initPages() {
+        START_PAGE = new StartPage();
+    }
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        initPages();
+
+        START_PAGE.show();
+
+        START_PAGE.getSubmitButton().addActionListener(e -> {
+            System.out.println(START_PAGE.getUsernameInput().getText());
+        });
     }
 }
